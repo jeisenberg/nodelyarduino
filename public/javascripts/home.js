d485bf04c162c3b8f2abb8753f1ffbdd0d59a6cc
Jack Eisenberg <jack@goshi.me>
2013-02-04 23:55:41 -0600

    added jq console

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,3 +1,31 @@
-function initConsole(){
-	
+
+var Console = function(){
+	var self = this;
+	var wrapper = $('#console');
+	self.terminal = wrapper.console({
+		promptLabel: 'Demo> ',
+		   commandValidate:function(line){
+		    if (line == "") return false;
+		       else return true;
+		    },
+		   commandHandle:function(line){
+		   		return [{msg:"=> [12,42]",
+		   		className:"jquery-console-message-value"},
+		   		{msg:":: [a]",
+		   		className:"jquery-console-message-type"}]
+		   	},
+		   autofocus:true,
+		   animateScroll:true,
+		   promptHistory:true,
+		   charInsertTrigger:function(keycode,line){
+		              // Let you type until you press a-z
+		              // Never allow zero.
+		   //return !line.match(/[a-z]+/) && keycode != '0'.charCodeAt(0);
+			return keycode;
+		    }
+		});
 }
+
+$(function(){
+	var console = new Console();
+});
