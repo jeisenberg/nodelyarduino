bbbdf191415ce320f9871c953f29ed84080c40d8
Jack Eisenberg <jack@goshi.me>
2013-02-05 00:17:29 -0600

    downgrade jq, console fix

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,28 +2,17 @@
 var Console = function(){
 	var self = this;
 	var wrapper = $('#console');
-	self.terminal = wrapper.console({
-		promptLabel: 'Demo> ',
-		   commandValidate:function(line){
-		    if (line == "") return false;
-		       else return true;
-		    },
-		   commandHandle:function(line){
-		   		return [{msg:"=> [12,42]",
-		   		className:"jquery-console-message-value"},
-		   		{msg:":: [a]",
-		   		className:"jquery-console-message-type"}]
-		   	},
-		   autofocus:true,
-		   animateScroll:true,
-		   promptHistory:true,
-		   charInsertTrigger:function(keycode,line){
-		              // Let you type until you press a-z
-		              // Never allow zero.
-		   //return !line.match(/[a-z]+/) && keycode != '0'.charCodeAt(0);
-			return keycode;
-		    }
-		});
+	self.terminal = wrapper.jqconsole('Hi\n', '>>>');
+	self.startPrompt = function(){
+	          // Start the prompt with history enabled.
+	          self.terminal.Prompt(true, function (input) {
+	            // Output input with the class jqconsole-output.
+	          	self.terminal.Write(input + '\n', 'jqconsole-output');
+	            // Restart the prompt.
+							self.startPrompt();
+						});
+	}
+	self.startPrompt();
 }
 
 $(function(){
